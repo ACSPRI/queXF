f963197b230b832c7a502c9ebe54bc733a003dca
azammitdcarf <azammitdcarf>
2009-04-24 01:27:44 +0000

    Updated to queXF

--- lang.inc.php
+++ lang.inc.php
@@ -3,28 +3,28 @@
  * Language configuration file
  *
  *
- *	This file is part of queXS
+ *	This file is part of queXF
  *	
- *	queXS is free software; you can redistribute it and/or modify
+ *	queXF is free software; you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
  *	the Free Software Foundation; either version 2 of the License, or
  *	(at your option) any later version.
  *	
- *	queXS is distributed in the hope that it will be useful,
+ *	queXF is distributed in the hope that it will be useful,
  *	but WITHOUT ANY WARRANTY; without even the implied warranty of
  *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *	GNU General Public License for more details.
  *	
  *	You should have received a copy of the GNU General Public License
- *	along with queXS; if not, write to the Free Software
+ *	along with queXF; if not, write to the Free Software
  *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  *
  * @author Adam Zammit <adam.zammit@deakin.edu.au>
  * @copyright Deakin University 2007,2008
- * @package queXS
+ * @package queXF
  * @subpackage configuration
- * @link http://www.deakin.edu.au/dcarf/ queXS was writen for DCARF - Deakin Computer Assisted Research Facility
+ * @link http://www.deakin.edu.au/dcarf/ queXF was writen for DCARF - Deakin Computer Assisted Research Facility
  * @license http://opensource.org/licenses/gpl-2.0.php The GNU General Public License (GPL) Version 2
  * 
  */
